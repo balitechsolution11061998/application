678b1d5e1c9ad88f5b47f52d85e3e239d119ca0c
sulaksana234 <sulaksana60@gmail.com>
2024-08-04 09:33:30 +0800

    update

--- app/Events/IzinRequestCreated.php
+++ app/Events/IzinRequestCreated.php
@@ -4,22 +4,42 @@ namespace App\Events;
 
 use App\Models\Izin;
 use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\Broadcastable;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
 class IzinRequestCreated
 {
-    use Dispatchable, SerializesModels;
+    use Dispatchable, InteractsWithSockets, SerializesModels;
 
     public $izin;
 
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
     public function __construct(Izin $izin)
     {
         $this->izin = $izin;
     }
 
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return Channel|array
+     */
     public function broadcastOn()
     {
-        return new Channel('notifications');
+        return new Channel('izin-notifications');
+    }
+
+    public function broadcastWith()
+    {
+        return [
+            'izin' => $this->izin
+        ];
     }
 }
--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -1,18 +1,19 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" {!! printHtmlAttributes('html') !!}>
 <!--begin::Head-->
+
 <head>
-    <base href=""/>
+    <base href="" />
     <title>{{ config('settings.KT_APP_NAME') }} - @yield('title')</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta charset="utf-8"/>
-    <meta name="description" content=""/>
-    <meta name="keywords" content=""/>
-    <meta name="viewport" content="width=device-width, initial-scale=1"/>
-    <meta property="og:locale" content="en_US"/>
-    <meta property="og:type" content="article"/>
-    <meta property="og:title" content=""/>
-    <link rel="canonical" href=""/>
+    <meta charset="utf-8" />
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta property="og:locale" content="en_US" />
+    <meta property="og:type" content="article" />
+    <meta property="og:title" content="" />
+    <link rel="canonical" href="" />
 
     {!! includeFavicon() !!}
 
@@ -21,19 +22,19 @@
     <!--end::Fonts-->
 
     <!--begin::Global Stylesheets Bundle(used by all pages)-->
-    @foreach(getGlobalAssets('css') as $path)
+    @foreach (getGlobalAssets('css') as $path)
         {!! sprintf('<link rel="stylesheet" href="%s">', asset($path)) !!}
     @endforeach
     <!--end::Global Stylesheets Bundle-->
 
     <!--begin::Vendor Stylesheets(used by this page)-->
-    @foreach(getVendors('css') as $path)
+    @foreach (getVendors('css') as $path)
         {!! sprintf('<link rel="stylesheet" href="%s">', asset($path)) !!}
     @endforeach
     <!--end::Vendor Stylesheets-->
 
     <!--begin::Custom Stylesheets(optional)-->
-    @foreach(getCustomCss() as $path)
+    @foreach (getCustomCss() as $path)
         {!! sprintf('<link rel="stylesheet" href="%s">', asset($path)) !!}
     @endforeach
     <!--end::Custom Stylesheets-->
@@ -47,9 +48,11 @@
     <link rel="stylesheet" href="{{ asset('css/leaflet.css') }}">
     <link rel="stylesheet" href="{{ asset('css/jquery.fancybox.min.css') }}">
     <link href="https://fonts.googleapis.com/css2?family=Evergreen+Memories&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha384-k6RqeWeci5ZR/Lv4MR0sA0FfDOMhXoq9Z2R9rFmgLfu7v5OfmwLf1j5R93Xy5tD" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+        integrity="sha384-k6RqeWeci5ZR/Lv4MR0sA0FfDOMhXoq9Z2R9rFmgLfu7v5OfmwLf1j5R93Xy5tD" crossorigin="anonymous">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/5.10.1/main.min.css" rel="stylesheet" />
-    <link href="{{ asset('assets/plugins/custom/fullcalendar/fullcalendar.bundle.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/plugins/custom/fullcalendar/fullcalendar.bundle.css') }}" rel="stylesheet"
+        type="text/css" />
     <link href="https://cdn.jsdelivr.net/npm/@fancyapps/ui/dist/fancybox.css" rel="stylesheet">
 
     <!--end::Javascript-->
@@ -57,85 +60,107 @@
 <!--end::Head-->
 
 <!--begin::Body-->
+
 <body {!! printHtmlClasses('body') !!} {!! printHtmlAttributes('body') !!}>
 
-@include('partials/theme-mode/_init')
+    @include('partials/theme-mode/_init')
 
-@yield('content')
+    @yield('content')
 
-<!--begin::Javascript-->
-<!--begin::Global Javascript Bundle(mandatory for all pages)-->
-@foreach(getGlobalAssets() as $path)
-    {!! sprintf('<script src="%s"></script>', asset($path)) !!}
-@endforeach
-<!--end::Global Javascript Bundle-->
+    <!--begin::Javascript-->
+    <!--begin::Global Javascript Bundle(mandatory for all pages)-->
+    @foreach (getGlobalAssets() as $path)
+        {!! sprintf('<script src="%s"></script>', asset($path)) !!}
+    @endforeach
+    <!--end::Global Javascript Bundle-->
 
-<!--begin::Vendors Javascript(used by this page)-->
-@foreach(getVendors('js') as $path)
-    {!! sprintf('<script src="%s"></script>', asset($path)) !!}
-@endforeach
-<!--end::Vendors Javascript-->
+    <!--begin::Vendors Javascript(used by this page)-->
+    @foreach (getVendors('js') as $path)
+        {!! sprintf('<script src="%s"></script>', asset($path)) !!}
+    @endforeach
+    <!--end::Vendors Javascript-->
 
-<!--begin::Custom Javascript(optional)-->
-@foreach(getCustomJs() as $path)
-    {!! sprintf('<script src="%s"></script>', asset($path)) !!}
-@endforeach
+    <!--begin::Custom Javascript(optional)-->
+    @foreach (getCustomJs() as $path)
+        {!! sprintf('<script src="%s"></script>', asset($path)) !!}
+    @endforeach
 
-<script> var hostUrl = "{{ asset('/assets') }}/";        </script>
+    <script>
+        var hostUrl = "{{ asset('/assets') }}/";
+    </script>
 
-{{-- custome js --}}
-<script src="{{ asset('js/intro.min.js') }}"></script>
-<script src="{{ asset('js/toastr.min.js') }}"></script>
-<script src="{{ asset('js/toastify-js.js') }}"></script>
-<script src="{{ asset('js/leaflet.js') }}"></script>
-<script src="{{ asset('js/jquery.validate.min.js') }}"></script>
-<script src="{{ asset('js/jquery.fancybox.min.js') }}"></script>
-<script src="{{ asset('js/tourguide.js') }}"></script>
+    {{-- custome js --}}
+    <script src="{{ asset('js/intro.min.js') }}"></script>
+    <script src="{{ asset('js/toastr.min.js') }}"></script>
+    <script src="{{ asset('js/toastify-js.js') }}"></script>
+    <script src="{{ asset('js/leaflet.js') }}"></script>
+    <script src="{{ asset('js/jquery.validate.min.js') }}"></script>
+    <script src="{{ asset('js/jquery.fancybox.min.js') }}"></script>
+    <script src="{{ asset('js/tourguide.js') }}"></script>
 
-<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-<script src="https://js.pusher.com/7.0/pusher.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/laravel-echo/1.11.3/echo.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/apexcharts@latest"></script>
-<script src="{{ asset('assets/plugins/custom/fullcalendar/fullcalendar.bundle.js') }}"></script>
-<script src="https://cdn.jsdelivr.net/npm/@fancyapps/ui/dist/fancybox.umd.js"></script>
-
-
-<!--end::Custom Javascript-->
-@stack('scripts')
-<!--end::Javascript-->
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script src="https://js.pusher.com/7.0/pusher.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/laravel-echo/1.11.3/echo.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/apexcharts@latest"></script>
+    <script src="{{ asset('assets/plugins/custom/fullcalendar/fullcalendar.bundle.js') }}"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@fancyapps/ui/dist/fancybox.umd.js"></script>
 
 
-<script>
-    let chart;
-    var calendar;
+    <!--end::Custom Javascript-->
+    @stack('scripts')
+    <!--end::Javascript-->
 
-    document.addEventListener('livewire:load', () => {
-        Livewire.on('success', (message) => {
-            toastr.success(message);
-        });
-        Livewire.on('error', (message) => {
-            toastr.error(message);
-        });
 
-        Livewire.on('swal', (message, icon, confirmButtonText) => {
-            if (typeof icon === 'undefined') {
-                icon = 'success';
-            }
-            if (typeof confirmButtonText === 'undefined') {
-                confirmButtonText = 'Ok, got it!';
-            }
-            Swal.fire({
-                text: message,
-                icon: icon,
-                buttonsStyling: false,
-                confirmButtonText: confirmButtonText,
-                customClass: {
-                    confirmButton: 'btn btn-primary'
+    <script>
+        let chart;
+        var calendar;
+
+        document.addEventListener('livewire:load', () => {
+            Livewire.on('success', (message) => {
+                toastr.success(message);
+            });
+            Livewire.on('error', (message) => {
+                toastr.error(message);
+            });
+
+            Livewire.on('swal', (message, icon, confirmButtonText) => {
+                if (typeof icon === 'undefined') {
+                    icon = 'success';
                 }
+                if (typeof confirmButtonText === 'undefined') {
+                    confirmButtonText = 'Ok, got it!';
+                }
+                Swal.fire({
+                    text: message,
+                    icon: icon,
+                    buttonsStyling: false,
+                    confirmButtonText: confirmButtonText,
+                    customClass: {
+                        confirmButton: 'btn btn-primary'
+                    }
+                });
             });
         });
-    });
-</script>
+    </script>
+    <script>
+        Pusher.logToConsole = true;
+        var chartInstance = null;
+        var pusher = new Pusher('200a3ba86105ed6ce25f', {
+            cluster: 'ap1'
+        });
+        var channel = pusher.subscribe('izin-notifications');
+
+        // Listen for the event
+        channel.bind('App\\Events\\IzinRequestCreated', function(data) {
+            console.log('Notification received:', data.izin);
+            // Show the notification
+            new Notification('New Izin Request', {
+                body: `Izin Code: ${data.izin.kode_izin}\nFrom: ${data.izin.tgl_izin_dari}\nTo: ${data.izin.tgl_izin_sampai}`,
+                icon: '/image/logo.png' // optional
+            });
+
+        });
+    </script>
 </body>
 <!--end::Body-->
 
