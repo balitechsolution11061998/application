26b1c89d571d3410393a56f04564a2dfab76c726
sulaksana234 <sulaksana60@gmail.com>
2024-08-16 14:16:47 +0800

    update

--- resources/views/layout/partials/sidebar-layout/_footer.blade.php
+++ resources/views/layout/partials/sidebar-layout/_footer.blade.php
@@ -1,4 +1,4 @@
-<!--begin::Footer-->
+{{-- <!--begin::Footer-->
 <div id="kt_app_footer" class="app-footer">
 	<!--begin::Footer container-->
 	<div class="app-container container-fluid d-flex flex-column flex-md-row flex-center flex-md-stack py-3">
@@ -14,19 +14,8 @@
 			<span class="text-muted fw-semibold me-1">                    {{ config('settings.KT_APP_NAME_' . (Auth::user() && Auth::user()->hasRole('karyawan') ? 'E_PRESENSI' : (Auth::user() && Auth::user()->hasRole('siswa') ? 'CBT' : 'BAYU_SYS'))) }}
             </span>
 		</div>
-		{{-- <ul class="menu menu-gray-600 menu-hover-primary fw-semibold order-1">
-			<li class="menu-item">
-				<a href="https://keenthemes.com" target="_blank" class="menu-link px-2">About</a>
-			</li>
-			<li class="menu-item">
-				<a href="https://devs.keenthemes.com" target="_blank" class="menu-link px-2">Support</a>
-			</li>
-			<li class="menu-item">
-				<a href="https://1.envato.market/EA4JP" target="_blank" class="menu-link px-2">Purchase</a>
-			</li>
-		</ul> --}}
 		<!--end::Menu-->
 	</div>
 	<!--end::Footer container-->
 </div>
-<!--end::Footer-->
+<!--end::Footer--> --}}
