fcbcd3f00242749d51a0068697b494246e4fd5be
sulaksana234 <sulaksana60@gmail.com>
2024-08-22 16:05:33 +0800

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -217,8 +217,11 @@
             </div>
         </div>
     </div>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" defer></script>
     <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+    <script src="{{ asset('js/toastr.min.js') }}" defer></script>
+    <script src="{{ asset('js/toastify-js.js') }}" defer></script>
+
     <script src="{{ asset('js/toastr.min.js') }}"></script>
     <script src="{{ asset('js/toastify-js.js') }}"></script>
     <script>
