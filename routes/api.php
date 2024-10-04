e8a7bfc3195476bdd5ae2eb01193364232043c5c
sulaksana234 <sulaksana60@gmail.com>
2024-10-04 15:00:09 +0800

    update

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\PoController;
+use App\Http\Controllers\ItemSupplierController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -22,3 +23,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 
 Route::post('/po/store', [PoController::class, 'store']);
+Route::post('/itemsupplier/store', [ItemSupplierController::class, 'store']);
