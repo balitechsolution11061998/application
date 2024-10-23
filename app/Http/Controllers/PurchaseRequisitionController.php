79524dc1c505e4ec3d1fea7d9047d9c92c6c1e16
sulaksana234 <sulaksana60@gmail.com>
2024-10-23 08:49:44 +0800

    update

--- app/Http/Controllers/PurchaseRequisitionController.php
+++ app/Http/Controllers/PurchaseRequisitionController.php
@@ -14,7 +14,7 @@ class PurchaseRequisitionController extends Controller
     //
     public function store(Request $request)
     {
-        dd($request->all());
+        return $request->all();
         // DB::beginTransaction(); // Uncomment this to enable transactions if necessary
         try {
             // Get the last PurchaseRequisition ID
