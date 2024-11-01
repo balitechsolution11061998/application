bb30bcdc401129cf5cb1285156d04d29cfa6f386
sulaksana234 <sulaksana60@gmail.com>
2024-11-01 12:52:00 +0800

    update

--- database/migrations/2024_10_24_015131_create_rcv_table.php
+++ database/migrations/2024_10_24_015131_create_rcv_table.php
@@ -55,30 +55,31 @@ return new class extends Migration
         });
 
         Schema::create('temp_rcv', function (Blueprint $table) {
-            $table->bigIncrements('receive_no');
-            $table->date('receive_date');
-            $table->date('created_date');
-            $table->string('receive_id', 50);
-            $table->integer('order_no')->nullable();
-            $table->integer('ref_no')->nullable();
-            $table->string('order_type', 10)->nullable();
-            $table->string('status_ind', 5)->nullable();
-            $table->date('approval_date');
-            $table->string('approval_id', 50)->nullable();
-            $table->integer('store');
-            $table->string('store_name', 20);
-            $table->integer('sku');
-            $table->string('sku_desc', 191);
-            $table->string('upc', 20)->nullable();
-            $table->integer('qty_expected')->nullable();
-            $table->integer('qty_received')->nullable();
-            $table->double('unit_cost')->nullable();
-            $table->double('unit_retail')->nullable();
-            $table->double('vat_cost')->nullable();
-            $table->double('unit_cost_disc')->nullable();
-            $table->integer('supplier');
-            $table->string('sup_name', 191);
-            $table->string('comment_desc', 191)->nullable();
+			$table->integer('receive_no');
+			$table->date('receive_date');
+			$table->date('created_date');
+			$table->string('receive_id', 50);
+			$table->string('order_no')->nullable();
+			$table->integer('ref_no')->nullable();
+			$table->string('order_type', 10)->nullable();
+			$table->string('status_ind', 5)->nullable();
+			$table->date('approval_date');
+			$table->string('approval_id', 50)->nullable();
+			$table->integer('store');
+			$table->string('store_name', 20);
+			$table->integer('sku');
+			$table->string('sku_desc');
+			$table->string('upc', 20)->nullable();
+			$table->integer('qty_expected')->nullable();
+			$table->integer('qty_received')->nullable();
+			$table->integer('unit_cost')->nullable();
+			$table->integer('unit_retail')->nullable();
+			$table->float('vat_cost')->nullable();
+			$table->integer('unit_cost_disc')->nullable();
+			$table->integer('supplier');
+			$table->string('sup_name');
+			$table->string('comment_desc')->nullable();
+            $table->timestamps();
         });
     }
 
