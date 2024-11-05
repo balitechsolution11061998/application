9cefd2e3278277682a4b4523f85049718c4f0025
sulaksana234 <sulaksana60@gmail.com>
2024-11-05 17:30:58 +0800

    update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,7 +11,7 @@ class HomeController extends Controller
 
     public function index()
     {
-        $loggedInUsers = User::join('authentications_monitoring', 'users.id', '=', 'authentications_monitoring.user_id')
+        $loggedInUsers = User::leftJoin('authentications_monitoring', 'users.id', '=', 'authentications_monitoring.user_id')
         ->where('users.is_logged_in', true)
         ->where('authentications_monitoring.action_type', 'login')
         ->select('users.*', 'authentications_monitoring.*')
