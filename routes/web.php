09f9bdb60b21a5be06a074beb2ae2592b8f5218b
sulaksana234 <sulaksana60@gmail.com>
2024-12-01 08:24:35 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -23,214 +23,126 @@ use App\Http\Controllers\TpsController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+// Web Routes
+Route::get('/', fn() => view('welcome'));
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Authentication Routes
 Route::controller(RegisterController::class)->group(function () {
     Route::get('register', 'showRegisterForm')->name('register');
     Route::post('register', 'register');
 });
 
-Route::get('/activities/data', [ActivityController::class, 'getData'])->name('activities.data');
-Route::get('/get-ram-usage', [SystemUsageController::class, 'getRamUsageData'])->name('ramUsageData');
+Route::controller(LoginController::class)->group(function () {
+    Route::get('login/form', 'showLoginForm')->name('login.form');
+    Route::post('login/prosesForm', 'login')->name('login.prosesForm');
+    Route::post('logout', 'logout')->name('logout');
+});
 
-Route::get('login/form', [LoginController::class, 'showLoginForm'])->name('login.form');
-Route::post('login/prosesForm', [LoginController::class, 'login'])->name('login.prosesForm');
-Route::post('logout', [LoginController::class, 'logout'])->name('logout');
-// Home route with 'auth' middleware and role check (Laratrust)
-Route::get('/home', [HomeController::class, 'index'])
-    ->name('home')
-    ->middleware('auth');
-
-// Grouping Management User routes under 'management/users'
-Route::prefix('/users')
-    ->middleware('auth')
-    ->as('users.') // Set a name prefix for all routes in this group
-    ->group(function () {
-        // User Management Dashboard
-        Route::get('/', [UserController::class, 'index'])
-            ->name('index'); // 'management.users.index'
-
-        // Fetching user data for DataTables
-        Route::get('/data', [UserController::class, 'getUsersData'])
-            ->name('data'); // 'management.users.data'
-
-        // Store user data (create/update)
-        Route::post('/store', [UserController::class, 'store'])
-            ->name('store'); // 'management.users.store'
-
-        // Edit user data by ID
-        Route::get('/{id}/edit', [UserController::class, 'edit'])
-            ->name('edit'); // 'management.users.edit'
-
-        // Delete user data by ID
-        Route::delete('/{id}', [UserController::class, 'destroy'])
-            ->name('destroy'); // 'management.users.destroy'
-
-        // Change user password
-        Route::post('/change-password', [UserController::class, 'changePassword'])
-            ->name('changePassword'); // 'management.users.changePassword'
-
-        // User profile
-        Route::get('/profile', [UserController::class, 'profile'])
-            ->name('profile'); // 'management.users.profile'
-
-        Route::post('/add-email', [UserController::class, 'addEmail'])
-            ->name('add-email'); // 'management.users.changePassword'
-
-        Route::post('/delete-email', [UserController::class, 'deleteEmail'])
-            ->name('delete-email'); // 'management.users.changePassword'
-
-        Route::post('/rolesdelete', [UserController::class, 'deleteRole'])
-            ->name('deleteRole'); // 'management.users.changePassword'
+Route::get('/koperasi', fn() => view('koperasi'));
 
 
+// Home Route
+
+// Grouping Authenticated Routes
+Route::middleware('auth')->group(function () {
+
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+    // User Management Routes
+    Route::prefix('users')->as('users.')->group(function () {
+        Route::get('/', [UserController::class, 'index'])->name('index');
+        Route::get('/data', [UserController::class, 'getUsersData'])->name('data');
+        Route::post('/store', [UserController::class, 'store'])->name('store');
+        Route::get('/{id}/edit', [UserController::class, 'edit'])->name('edit');
+        Route::delete('/{id}', [UserController::class, 'destroy'])->name('destroy');
+        Route::post('/change-password', [UserController::class, 'changePassword'])->name('changePassword');
+        Route::get('/profile', [UserController::class, 'profile'])->name('profile');
+        Route::post('/add-email', [UserController::class, 'addEmail'])->name('add-email');
+        Route::post('/delete-email', [UserController::class, 'deleteEmail'])->name('delete-email');
+        Route::post('/rolesdelete', [UserController::class, 'deleteRole'])->name('deleteRole');
         Route::get('/{id}/addstore', [UserController::class, 'addStore'])->name('addstore');
         Route::get('/{id}/addemail', [UserController::class, 'addEmailView'])->name('addEmailView');
         Route::get('/{id}/formUser', [UserController::class, 'formUser'])->name('formUser');
-
-
     });
 
-Route::prefix('dashboard-pilkada')
-    ->middleware('auth')
-    ->as('dashboard-pilkada.')
-    ->group(function () {
-        // Dashboard Pilkada main route
+    // Dashboard Pilkada Routes
+    Route::prefix('dashboard-pilkada')->as('dashboard-pilkada.')->group(function () {
         Route::get('/', [DashboardPilkadaController::class, 'index'])->name('index');
-
-        // Additional routes related to Dashboard Pilkada
         Route::get('/fetchData', [DashboardPilkadaController::class, 'fetchData'])->name('fetchData');
     });
 
-
-
-// Role management routes
-Route::prefix('roles')
-    ->middleware('auth')
-    ->as('roles.')
-    ->group(function () {
-        // Fetch roles data
+    // Role Management Routes
+    Route::prefix('roles')->as('roles.')->group(function () {
         Route::get('/getRoles', [RoleController::class, 'getRoles'])->name('getRoles');
         Route::get('/data', [RoleController::class, 'data'])->name('data');
-
-        // Standard CRUD routes for Roles
         Route::resource('/', RoleController::class)->parameters(['' => 'role'])->except(['show']);
     });
 
+    // Tahun Pelajaran Routes
+    Route::resource('tahun-pelajaran', TahunPelajaranController::class);
+    Route::get('tahun-pelajaran/data', [TahunPelajaranController::class, 'getData'])->name('tahun-pelajaran.getData');
 
-
-Route::resource('tahun-pelajaran', TahunPelajaranController::class);
-Route::get('tahun-pelajaran/data', [TahunPelajaranController::class, 'getData'])->name('tahun-pelajaran.getData');
-
-Route::prefix('rooms')
-    ->middleware('auth')
-    ->as('rooms.')
-    ->group(function () {
-        // Additional endpoints for fetching room data if needed
+    // Room Management Routes
+    Route::prefix('rooms')->as('rooms.')->group(function () {
         Route::get('/data', [RoomController::class, 'data'])->name('data');
-
-        // Standard CRUD routes for Rooms, excluding show route
-        Route::resource('/', RoomController::class)
-            ->parameters(['' => 'room'])
-            ->except(['show']);
+        Route::resource('/', RoomController::class)->parameters(['' => 'room'])->except(['show']);
     });
 
-Route::prefix('permissions')
-    ->middleware(['auth'])  // Middleware to ensure the user is authenticated
-    ->as('permissions.')    // Route name prefix for easier reference
-    ->group(function () {
+    // Permission Management Routes
+    Route::prefix('permissions')->as('permissions.')->group(function () {
         Route::resource('/', PermissionController::class)->except(['show'])->parameters(['' => 'permission']);
-
-        // Additional route for DataTables AJAX request
         Route::get('/data', [PermissionController::class, 'data'])->name('data');
     });
 
-
-Route::prefix('suppliers')
-    ->middleware(['auth']) // Ensure user authentication
-    ->as('suppliers.')     // Route name prefix for suppliers
-    ->group(function () {
-        // Route for importing suppliers via CSV/XLSX
+    // Supplier Management Routes
+    Route::prefix('suppliers')->as('suppliers.')->group(function () {
         Route::post('/import', [UserController::class, 'import'])->name('import');
     });
 
-Route::prefix('stores')
-    ->middleware(['auth']) // Ensure user authentication
-    ->as('stores.')     // Route name prefix for suppliers
-    ->group(function () {
-        // Route for importing suppliers via CSV/XLSX
+    // Store Management Routes
+    Route::prefix('stores')->as('stores.')->group(function () {
         Route::post('/storeUser', [StoreController::class, 'storeUser'])->name('storeUser');
         Route::post('/deleteStoreUser', [StoreController::class, 'deleteStoreUser'])->name('deleteStoreUser');
-
     });
 
-Route::prefix('regions')
-    ->middleware('auth')
-    ->as('regions.') // Set a name prefix for all role routes
-    ->group(function () {
-        // Fetch roles data
-        Route::get('/data', [RegionController::class, 'data'])
-            ->name('data'); // 'roles.getRoles'
+    // Region Management Routes
+    Route::prefix('regions')->as('regions.')->group(function () {
+        Route::get('/data', [RegionController::class, 'data'])->name('data');
     });
 
-Route::prefix('purchase-orders') // Prefix for all order routes
-    ->middleware('auth') // Ensure the user is authenticated
-    ->as('purchase-orders.') // Prefix for route names
-    ->group(function () {
-        // Fetch orders data
+    // Purchase Orders Routes
+    Route::prefix('purchase-orders')->as('purchase-orders.')->group(function () {
         Route::get('/getOrders', [OrderController::class, 'getOrders'])->name('getOrders');
         Route::get('/data', [OrderController::class, 'data'])->name('data');
-
-        // Standard CRUD routes for Orders
         Route::resource('/', OrderController::class)->parameters(['' => 'order'])->except(['show']);
-        Route::post('/store', [OrderController::class, 'store']);
-        Route::get('/edit/{id}', [OrderController::class, 'edit'])->name('edit');
-        Route::get('/show/{id}', [OrderController::class, 'show'])->name('show');
-
     });
 
-// Profile Picture management routes
-Route::post('/upload-profile-picture', [ProfileController::class, 'uploadProfilePicture']);
-Route::post('/remove-profile-picture', [ProfileController::class, 'removePicture'])
-    ->name('remove.profile.picture');
+    // Profile Picture Management Routes
+    Route::post('/upload-profile-picture', [ProfileController::class, 'uploadProfilePicture']);
+    Route::post('/remove-profile-picture', [ProfileController::class, 'removePicture'])->name('remove.profile.picture');
 
-// Verify Superadmin password route
-Route::post('/verify-superadmin-password', [UserController::class, 'verifySuperadminPassword'])
-    ->name('management.verifySuperadminPassword');
-Route::get('/docs', function () {
-    return view('docs.index'); // File Markdown bisa Anda buat di sini
+    // Superadmin Verification Route
+    Route::post('/verify-superadmin-password', [UserController::class, 'verifySuperadminPassword'])->name('management.verifySuperadminPassword');
+
+    // Sync Routes for Provinces, Kabupaten, Kecamatan, Kelurahan, TPS, and Pilkada
+    Route::get('/sync-provinces', [ProvinceController::class, 'syncProvinces']);
+    Route::get('/data-provinces', [ProvinceController::class, 'dataProvinces']);
+    Route::get('/sync-kabupaten/{province}', [KabupatenController::class, 'syncKabupaten']);
+    Route::get('/data-kabupaten', [KabupatenController::class, 'dataKabupaten']);
+    Route::get('/sync-kecamatan/{provinceId}/{kabupatenId}', [KecamatanController::class, 'syncKecamatan']);
+    Route::get('/data-kecamatan', [KecamatanController::class, 'dataKecamatan']);
+    Route::get('/sync-kelurahan/{provinceId}/{kabupatenId}/{kecamatanId}', [KelurahanController::class, 'syncKelurahan']);
+    Route::get('/data-kelurahan', [KelurahanController::class, 'dataKelurahan']);
+    Route::get('/sync-tps/{provinceId}/{kabupatenId}/{kecamatanId}/{kelurahanId}', [TpsController::class, 'syncTps']);
+    Route::get('/data-tps', [TpsController::class, 'dataTps']);
+    Route::get('/sync-pilkada/{provinceId}/{kabupatenId}/{kecamatanId}/{kelurahanId}/{tpsId}', [SyncDataController::class, 'syncDataPemilihan']);
+    Route::get('/data-pilkada', [SyncDataController::class, 'dataPilkada']);
 });
+
+// Documentation Route
+Route::get('/docs', fn() => view('docs.index'));
+
+// Laravel File Manager Routes
 Route::group(['prefix' => 'laravel-filemanager', 'middleware' => ['web', 'auth']], function () {
     \UniSharp\LaravelFilemanager\Lfm::routes();
 });
-Route::get('/sync-provinces', [ProvinceController::class, 'syncProvinces']);
-Route::get('/data-provinces', [ProvinceController::class, 'dataProvinces']);
-
-Route::get('/sync-kabupaten/{province}', [KabupatenController::class, 'syncKabupaten']);
-Route::get('/data-kabupaten', [KabupatenController::class, 'dataKabupaten']);
-
-
-Route::get('/sync-kecamatan/{provinceId}/{kabupatenId}', [KecamatanController::class, 'syncKecamatan']);
-Route::get('/data-kecamatan', [KecamatanController::class, 'dataKecamatan']);
-
-
-Route::get('/sync-kelurahan/{provinceId}/{kabupatenId}/{kecamatanId}', [KelurahanController::class, 'syncKelurahan']);
-Route::get('/data-kelurahan', [KelurahanController::class, 'dataKelurahan']);
-
-Route::get('/sync-tps/{provinceId}/{kabupatenId}/{kecamatanId}/{kelurahanId}', [TpsController::class, 'syncTps']);
-Route::get('/data-tps', [TpsController::class, 'dataTps']);
-
-Route::get('/sync-pilkada/{provinceId}/{kabupatenId}/{kecamatanId}/{kelurahanId}/{tpsId}', [SyncDataController::class, 'syncDataPemilihan']);
-Route::get('/data-pilkada', [SyncDataController::class, 'dataPilkada']);
