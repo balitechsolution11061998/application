397daeff732d982c12dddeb21e819a688a523419
sulaksana234 <sulaksana60@gmail.com>
2024-12-02 15:50:46 +0800

    udpate

--- app/Repositories/OrdSku/OrdSkuRepositoryImplement.php
+++ app/Repositories/OrdSku/OrdSkuRepositoryImplement.php
@@ -23,15 +23,18 @@ class OrdSkuRepositoryImplement extends Eloquent implements OrdSkuRepository{
 
     public function updateOrCreate($data)
     {
-        return $this->model->updateOrCreate(
-            [
-                'ordhead_id' => $data['ordhead_id'],
-                'sku' => $data['sku'],
-                'order_no' => $data['order_no']
-            ],
-            [
+        // Check if the record already exists
+        $existingRecord = $this->model->where([
+            'ordhead_id' => $data['ordhead_id'],
+            'sku' => $data['sku'],
+            'order_no' => $data['order_no'],
+            'upc' => $data['upc'],
+        ])->first();
+
+        if ($existingRecord) {
+            // Update the existing record
+            return $existingRecord->update([
                 'sku_desc' => $data['sku_desc'],
-                'upc' => $data['upc'],
                 'tag_code' => $data['tag_code'],
                 'unit_cost' => $data['unit_cost'],
                 'unit_retail' => $data['unit_retail'],
@@ -44,7 +47,25 @@ class OrdSkuRepositoryImplement extends Eloquent implements OrdSkuRepository{
                 'purchase_uom' => $data['purchase_uom'],
                 'supp_pack_size' => $data['supp_pack_size'],
                 'permanent_disc_pct' => $data['permanent_disc_pct'],
-            ]
-        );
+            ]);
+        } else {
+            // Create a new record
+            return $this->model->create(array_merge($data, [
+                'sku_desc' => $data['sku_desc'],
+                'tag_code' => $data['tag_code'],
+                'unit_cost' => $data['unit_cost'],
+                'unit_retail' => $data['unit_retail'],
+                'vat_cost' => $data['vat_cost'],
+                'luxury_cost' => $data['luxury_cost'],
+                'qty_ordered' => $data['qty_ordered'],
+                'qty_received' => $data['qty_received'],
+                'unit_discount' => $data['unit_discount'],
+                'unit_permanent_discount' => $data['unit_permanent_discount'],
+                'purchase_uom' => $data['purchase_uom'],
+                'supp_pack_size' => $data['supp_pack_size'],
+                'permanent_disc_pct' => $data['permanent_disc_pct'],
+            ]));
+        }
     }
+
 }
