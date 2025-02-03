4463ecad48ff55c32f2092649ab22698cdc14d26
sulaksana234 <sulaksana60@gmail.com>
2025-02-03 15:52:09 +0800

    update

--- config/laratrust_seeder.php
+++ config/laratrust_seeder.php
@@ -16,6 +16,7 @@ return [
             'dashboard-system' => 's',
             'data-kependudukan' => 's',
             'dashboard-pilkada' => 's',
+            'dashboard-po' => 's',
             'activity-log' => 's',
             'price-change' => 's',
             'purchase-order' => 's',
