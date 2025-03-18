8e8e03354d61fa0759631e334e09502c180bc95c
sulaksana234 <sulaksana60@gmail.com>
2025-03-18 07:36:49 +0000

    update

--- app/Http/Controllers/Api/RcvController.php
+++ app/Http/Controllers/Api/RcvController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\RcvHead;
 use Illuminate\Http\Request;
 use App\Services\Rcv\RcvService;
+use Illuminate\Support\Facades\Auth;
 
 class RcvController extends Controller
 {
@@ -14,7 +16,49 @@ class RcvController extends Controller
     {
         $this->rcvService = $rcvService;
     }
+    public function getData(Request $request)
+    {
+        try {
+            $user = Auth::user();
+            $receiveDate = $request->filterDate;
+            $data = [];
+            $count = 0;
 
+            // Increase the chunk size for faster processing, adjust as needed
+            $chunkSize = 1000;
+
+            // Start time for monitoring
+            $startTime = microtime(true);
+
+            RcvHead::with(['details'])->where('receive_date', $receiveDate)
+                ->chunk($chunkSize, function ($chunk) use (&$data, &$count, $startTime) {
+                    foreach ($chunk as $rcvHead) {
+                        $data[] = $rcvHead;
+                        $count++;
+
+                        // Check if we have reached 10,000 records or 10 seconds
+                        if ($count >= 10000 || (microtime(true) - $startTime) >= 10) {
+                            return false; // Stop chunking
+                        }
+                    }
+                });
+
+            return response()->json([
+                'title' => 'Sync Rcv Successfully',
+                'message' => 'Data Rcv Found',
+                'data' => $data,
+                'count' => $count,
+                'success' => true
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'title' => 'Error',
+                'message' => 'Failed to load data order',
+                'error' => $e->getMessage(),
+                'success' => false
+            ], 500);
+        }
+    }
     public function store(Request $request)
     {
         try {
