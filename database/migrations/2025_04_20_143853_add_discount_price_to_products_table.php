5116cd29b1f93ae0f88bc4afa42df7712220d6a6
sulaksana234 <sulaksana60@gmail.com>
2025-04-20 06:39:20 +0000

    update

---
+++ database/migrations/2025_04_20_143853_add_discount_price_to_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            // Add discount_price column
+            $table->decimal('discount_price', 10, 2)->nullable(); // Set nullable if you want to allow null values
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            // Drop the discount_price column if the migration is rolled back
+            $table->dropColumn('discount_price');
+        });
+    }
+};
